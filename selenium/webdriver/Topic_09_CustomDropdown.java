b70f8f73ed022cb2190721bd69260dbd29654fe0
ThanhNC <ThanhNC@DESKTOP-0U8FU7F>
2024-01-12 23:06:09 +0700

    tc1

--- selenium/webdriver/Topic_09_CustomDropdown.java
+++ selenium/webdriver/Topic_09_CustomDropdown.java
@@ -1,10 +1,14 @@
 package webdriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,7 +18,7 @@ public class Topic_09_CustomDropdown {
 	WebDriverWait explicitWait;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
-	
+
 	@BeforeClass
 	public void beforeClass() {
 		if (osName.contains("Windows")) {
@@ -22,19 +26,48 @@ public class Topic_09_CustomDropdown {
 		} else {
 			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
 		}
-		
+
 		driver = new FirefoxDriver();
 		explicitWait = new WebDriverWait(driver, 30);
-		
+
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
-	@Test
-	public void TC_01() {
-	}
-	
 
 	@Test
+	public void TC_01_JQuery() {
+		driver.get("http://jqueryui.com/resources/demos/selectmenu/default.html");
+		
+		SelectDroplist("span#speed-button", "ul#speed-menu div[role=\"option\"]", "Medium");
+		sleepInSecond(5);
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#speed-button span.ui-selectmenu-text")).getText(), "Medium");
+
+		SelectDroplist("span#speed-button", "ul#speed-menu div[role=\"option\"]", "Slower");
+		sleepInSecond(5);
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#speed-button span.ui-selectmenu-text")).getText(), "Slower");
+		
+		SelectDroplist("span#speed-button", "ul#speed-menu div[role=\"option\"]", "Faster");
+		sleepInSecond(5);
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#speed-button span.ui-selectmenu-text")).getText(), "Faster");
+		
+	}
+	
+	@Test
 	public void TC_02() {
+	}
+
+	public void SelectDroplist(String parentList, String item, String ExpectedText) {
+	
+		driver.findElement(By.cssSelector(parentList)).click();
+		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(parentList)));
+		
+		List<WebElement> SpeedDropdownlist =driver.findElements(By.cssSelector(item));
+		for (WebElement temptItem : SpeedDropdownlist) {
+			String itemText = temptItem.getText();
+			if (itemText.equals(ExpectedText)){
+				temptItem.click();
+			}
+			
+		}
 	}
 
 	public void sleepInSecond(long timeInSecond) {
@@ -45,6 +78,7 @@ public class Topic_09_CustomDropdown {
 			e.printStackTrace();
 		}
 	}
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
